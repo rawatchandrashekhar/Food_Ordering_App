239c11d65ffa0e26d869285398299421c9ad1ab0
rawatchandrashekhar <salesforce@centuryply.com>
2024-02-20 15:31:49 +0530

    commited after adding files into the gitignore

--- App.js
+++ App.js
@@ -1,2 +1,16 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
+
+const parent = React.createElement("div", { id: "parent" }, [
+    React.createElement("div", { id: "child" }, [
+        React.createElement("h1", {}, "This is namaste React"),
+        React.createElement("h2", {}, "by chandu"),
+    ]),
+    React.createElement("div", { id: "child2" }, [
+        React.createElement("h1", {}, "I am an H1 TAG"),
+        React.createElement("h2", {}, "I am an H2 TAG"),
+    ]),
+])
+
+const root = ReactDOM.createRoot(document.getElementById("root"))
+root.render(parent);
