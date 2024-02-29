e5f32f3a0521e72c07e2b390299a67f39f58a576
rawatchandrashekhar <salesforce@centuryply.com>
2024-02-29 18:55:24 +0530

    worked on app structure

--- App.js
+++ App.js
@@ -3,12 +3,58 @@ import ReactDOM from "react-dom/client";
 
 // React Element is an abject but when it renders into the DOM then it beacomes a HTML elements
 
-const parent = React.createElement("h1", { id: "heading" }, "Inside from the React");  // CORE REACT WILL CREATE THIS ELEMENT
+// const parent = React.createElement("h1", { id: "heading" }, "Inside from the React");  // CORE REACT WILL CREATE THIS ELEMENT
 
-const parentJSX = <h1 id="heading">PARENT JSX</h1>;
+// const parentJSX = <h1 id="heading">PARENT JSX</h1>;
+
+const Header = () => {
+    return (
+        <div className="header">
+            <div className="logo-container">
+                <img className="logo" src="https://seeklogo.com/images/F/foodx-online-food-ordering-system-logo-145CB16578-seeklogo.com.png" alt="logo" />
+            </div>
+            <div className="nav-items">
+                <ul>
+                    <li>Home</li>
+                    <li>About us</li>
+                    <li>Contact us</li>
+                    <li>Cart</li>
+                </ul>
+            </div>
+        </div>
+    )
+}
+
+const RestaurantCard = () => {
+    return (
+        <div className="res-card">
+            <h3>Meghna Foods</h3>
+        </div>
+    )
+}
+
+const Body = () => {
+    return (
+        <div className="body">
+            <div className="search">Search</div>
+            <div className="res-container">
+                <RestaurantCard />
+            </div>
+        </div>
+    )
+}
+
+const AppLayout = () => {
+    return (
+        <div className="app">
+            <Header />
+            <Body />
+        </div>
+    )
+}
 
 const root = ReactDOM.createRoot(document.getElementById("root"));  //ReactDOM will display created react element into the browser
-root.render(parentJSX);      // It will take an object and convert it into an HTML element and push it to the browser and it will replace everything whatever inside the "ROOT".
+root.render(<AppLayout />);      // It will take an object and convert it into an HTML element and push it to the browser and it will replace everything whatever inside the "ROOT".
 
 // const parent = React.createElement("div", { id: "parent" }, [
 //     React.createElement("div", { id: "child" }, [
